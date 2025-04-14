ece8438b294f0db506f5a2ca4883714660c44b54
Senne Cools <113857183+sennecools@users.noreply.github.com>
2025-04-14 09:57:02 +0200

    Init Backend folder
    Created the backend folder with express.js using typescript

---
+++ backend/src/index.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+const PORT = 3001;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/api/ping', (_req, res) => {
+	res.json({ message: 'pong 🏓' });
+});
+
+app.listen(PORT, () => {
+	console.log(`✅ Backend running at http://localhost:${PORT}`);
+});
